b5d021d5cc73452f39e3b628ca8261d3c8fa64bf
Jeferson Farias Calazans <calazans10@gmail.com>
2013-01-05 12:18:01 -0200

    configurações iniciais

--- eventex/settings.py
+++ eventex/settings.py
@@ -1,18 +1,20 @@
 # Django settings for eventex project.
+from unipath import Path
 
+PROJECT_DIR = Path(__file__).parent
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Jeferson Calazans', 'calazans10@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': PROJECT_DIR.child('database.db'),  # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
