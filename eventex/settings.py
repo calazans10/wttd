ff6378b49af06a8d3629ad32c86c30f29e448c6d
Jeferson Farias Calazans <calazans10@gmail.com>
2013-01-05 16:01:18 -0200

    refatorando a configuração do banco de dados

--- eventex/settings.py
+++ eventex/settings.py
@@ -10,11 +10,10 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-DATABASES['default'] = dj_database_url.config()
-# DATABASES = {
-    # 'default': dj_database_url.config(
-        # default='sqlite:///' + PROJECT_DIR.child('database.db'))
-# }
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 TIME_ZONE = 'America/Sao_Paulo'
 LANGUAGE_CODE = 'pt-br'
