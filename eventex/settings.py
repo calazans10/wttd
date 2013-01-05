91125835a4fdd6bc3e1cfae9c7dc6ed32a0178e5
Jeferson Farias Calazans <calazans10@gmail.com>
2013-01-05 16:17:23 -0200

    inserindo configurações de staticfiles no settings

--- eventex/settings.py
+++ eventex/settings.py
@@ -29,12 +29,11 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = PROJECT_DIR.child('public')
 STATIC_URL = '/static/'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+# A list of locations of additional static files
+STATICFILES_DIRS = ()
+
+# The default file storage backend used during the build process
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # List of finder classes that know how to find static files in
 # various locations.
