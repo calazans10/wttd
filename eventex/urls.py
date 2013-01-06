1070e09fc9565ae6fd2b40d85899262feccf3acd
Jeferson Farias Calazans <calazans10@gmail.com>
2013-01-06 18:38:44 -0200

    habilitando admin do django para inscrições

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,21 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^inscricao/', include('subscriptions.urls', namespace='subscriptions')),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'', include('core.urls', namespace='core')),
-
-    # Examples:
-    # url(r'^$', 'eventex.views.home', name='home'),
-    # url(r'^eventex/', include('eventex.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
