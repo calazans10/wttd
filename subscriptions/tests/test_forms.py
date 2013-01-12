5b039863cdd450790f63d75869e590f7c4b8a824
Jeferson Farias Calazans <calazans10@gmail.com>
2013-01-12 11:58:57 -0200

    adicionando migração e gerando mais testes de formulário

--- subscriptions/tests/test_forms.py
+++ subscriptions/tests/test_forms.py
@@ -24,9 +24,19 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(email='')
         self.assertFalse(form.errors)
 
+    def test_name_must_be_capitalized(self):
+        'Name must be capitalized'
+        form = self.make_validated_form(name='JEFERSON CALAZANS')
+        self.assertEqual('Jeferson Calazans', form.cleaned_data['name'])
+
+    def test_must_inform_email_or_phone(self):
+        'Email and Phone are option, but one must be informed.'
+        form = self.make_validated_form(email='', phone_0='', phone_1='')
+        self.assertItemsEqual(['__all__'], form.errors)
+
     def make_validated_form(self, **kwargs):
         data = dict(name='Jeferson Calazans', email='jeferson@exemplo.com',
-            cpf='12345678901', phone='21-9988-7766',)
+            cpf='12345678901', phone_0='21', phone_1='99887766',)
         data.update(kwargs)
         form = SubscriptionForm(data)
         form.is_valid()
